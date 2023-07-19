2225792beef65ad6ff5c3de6eedafe0122538789
Leo Zarazaga <lza1313@outlook.com>
2023-07-19 23:50:35 +0200

    Refactored code and added Mongoose for database integration.

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const userRouter = require('./routes/userRoutes')
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.json());
 
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     console.log('Hello from the middleware 👋');
     next();
 })
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next()
 })
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,6 +13,17 @@ exports.checkID = (req, res, next, val) => {
     next()
 }
 
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price'
+        })
+    }
+    next();
+}
+
+
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -41,7 +52,6 @@ exports.getTour = (req, res) => {
     })
 }
 
-
 exports.createTour = (req, res) => {
     // console.log(req.body);
 
@@ -49,7 +59,7 @@ exports.createTour = (req, res) => {
     const newTour = Object.assign({ id: newId }, req.body);
     tours.push(newTour)
 
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: 'success',
             data: {
