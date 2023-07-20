8cf9382186a17802900b57f69525da4408d536a6
Leo Zarazaga <lza1313@outlook.com>
2023-07-20 13:59:26 +0200

    More refactoring and understanding Postman better

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,72 +1,65 @@
-const fs = require('fs')
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+const Tour = require('../models/tourModel')
 
 
-exports.checkID = (req, res, next, val) => {
-    console.log(`Tour ID is ${val}`)
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find()
+
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        })
+
+    } catch (error) {
+        res.status(404).json({
             status: 'fail',
-            message: 'Invalid ID'
+            message: error
         })
     }
-    next()
 }
 
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(400).json({
+
+exports.getTour = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id)
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+
+    } catch (error) {
+        res.status(404).json({
             status: 'fail',
-            message: 'Missing name or price'
+            message: error
         })
     }
-    next();
 }
 
 
 
-exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
+exports.createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body)
 
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-}
-
-
-exports.getTour = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find((value) => value.id === id)
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-
-    })
-}
-
-exports.createTour = (req, res) => {
-    // console.log(req.body);
-
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    tours.push(newTour)
-
-    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: 'success',
             data: {
                 tour: newTour
             }
         })
-    })
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: 'Invalid data sent!'
+        })
+    }
 }
 
 
@@ -89,24 +82,3 @@ exports.deleteTour = (req, res) => {
 }
 
 
-
-
-/* exports.deleteTour = (req, res) => {
-    const id = parseInt(req.params.id);
-    const tour = tours.find((value) => value.id === id)
-
-    if (!tour) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-
-    const updatedTour = tours.filter((value) => value.id !== tour.id);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(updatedTour), err => {
-        res.status(204).json({
-            status: 'success',
-            data: null
-        })
-    })
-} */
--- server.js
+++ server.js
@@ -18,11 +18,6 @@ mongoose.connect(DB, {
 
 
 
-
-
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port: ${port}...`);
