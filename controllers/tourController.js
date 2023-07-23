e089654fc5638c56efb5865876d11ac8311f1a64
Leo Zarazaga <lza1313@outlook.com>
2023-07-23 23:01:12 +0200

    Making the API Better: Sorting. Understanding advance Postman querys

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,25 +3,40 @@ const Tour = require('../models/tourModel')
 
 
 exports.getAllTours = async (req, res) => {
+    console.log(req.query);
     try {
         //BUILD QUERY
-        const queryObj = {...req.query}
+
+        //1.A) FILTERING
+        const queryObj = { ...req.query }
         const excludedFields = ['page', 'sort', 'limit', 'fields']
         excludedFields.forEach((el => delete queryObj[el]))
+        // console.log(req.query, queryObj);
 
-        console.log(req.query, queryObj);
+        //1.B) ADVANCE FILTERING
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        
+            
+        //gte, gt, lte, lt
+        //>=,  > , <= , <
+        
+        let query = Tour.find(JSON.parse(queryStr))
+        
+        // 2) SORTING
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy)
+        }else{
+            query = query.sort('-createdAt')
+        }
 
-        const query =  Tour.find(queryObj)
-        // const query =  Tour.find({
-        //     duration: 5,
-        //     difficulty: 'easy'
-        // })
-
-        // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
-
+        
         //EXECUTE QUERY
         const tours = await query;
+        // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
 
+        
         //SEND RESPONSE
         res.status(200).json({
             status: 'success',
